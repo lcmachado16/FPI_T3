b23f6dd0219e005f31ab0ccedd5e6e07a8782663
lcmachado10 <lcmachado@inf.ufrgs.br>
2023-12-18 04:47:49 -0300

    falta opcao de salvar

--- main.cpp
+++ main.cpp
@@ -12,15 +12,36 @@ using namespace std;
 
 int lowThreshold = 0;
 const int max_lowThreshold = 100;
-const double ratio = 3;
-const int kernel_size = 3;
-const char* window_name = "Edge Map";
+const double const_ratio = 3;
+
+const char* window_name = "FPI UFRGS";
+const char* kernel_trackbar = "Kernel_Size::";
+const char* brightness_trackbar = "Brightness::";
+const char* contrast_trackbar = "Contrast::";
+
+
+int kernel_size = 3;
+
+//![variables]
+Mat src, src_gray;
+Mat dst, detected_edges;
 
 bool is_gray = false;
 bool is_gaussian_blur = false;
 bool is_hflip = false;
 bool is_vflip = false;
 bool is_canny = false;
+bool is_rotated = false;
+bool is_brilho = true;  // ATENCAO
+bool is_sobel = false;
+bool is_reduced = false;
+bool is_negative = false;
+
+static void on_trackbar( int, void* )
+{
+    cout << "CHomsky" << endl;
+}
+
 
 
 int main(int argc, char** argv)
@@ -28,36 +49,44 @@ int main(int argc, char** argv)
     int camera = 0;
     VideoCapture cap(0);
 
-       // Define a largura e a altura desejadas
-    int desired_width = 300;
-    int desired_height = 300;
-
-    // Define o tamanho do frame
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, desired_width);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, desired_height);
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
     if(!cap.open(camera))
         return 0;
+
+    // Cria trackbar para ajustar brilho
+    int iSliderValue1 = 0;
+    int iSliderValue2 = 50;
+
+
+
+    cv::namedWindow(window_name);
+    createTrackbar(kernel_trackbar, window_name, NULL, 32, 0);
+    createTrackbar(brightness_trackbar, window_name, NULL, 300, 0);
+    createTrackbar(contrast_trackbar, window_name, NULL, 300, 0);
+
     for(;;)
     {
         Mat frame;
         cap >> frame;
         if( frame.empty() ) break; // end of video stream
+        
+
+       
+
+
 
         Mat image;
         Mat img_gray;
         Mat img_blur; 
+        Mat img_canny;
+        Mat aux;
         cap.read(image);
         cvtColor(image,img_gray,COLOR_BGR2GRAY);
-        GaussianBlur(image, img_blur, Size(9,9),3,0);
 
-        //![variables]
-        Mat src, src_gray;
-        Mat dst, detected_edges;
 
         //blur( img_gray, detected_edges, Size(3,3) );
-        //Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+        //Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*const_ratio, kernel_size );
 
        // imshow("CHOMSKY", detected_edges);
 
@@ -66,25 +95,75 @@ int main(int argc, char** argv)
         // cv::resize(frame, resized, cv::Size(800, 600));
         // cv::imshow("resized",resized);
 
+
+        // image.convertTo(new_image, -1, alpha, beta);
+
+
+
         Mat output_image = image; 
         
 
         //cv::rotate(image, output_image, cv::ROTATE_90_CLOCKWISE);
 
+        if(is_rotated) {
+            rotate_90(output_image);
+        }
+
         if (is_gaussian_blur){
             GaussianBlur(output_image, output_image, Size(9,9),0,0);
         }
-        if (is_gray){
-            output_image = img_gray;
-        }
         output_image = is_gray ? img_gray : output_image ;
-
-        //hflip
         output_image = is_hflip ? hflip(output_image) : output_image ;
         output_image = is_vflip ? vflip(output_image) : output_image ;
+        if (is_negative) {
+            negative(output_image);
+        }
+        
+        if(is_canny){
+            aux = output_image;
+            Canny(aux,output_image, 50,150);
+        }
+          
+        if(is_brilho){
+            double contrast_value  = cv::getTrackbarPos(contrast_trackbar, window_name) + 100;
+            contrast_value = contrast_value / 100 ;
+            double brightness_value = cv::getTrackbarPos(brightness_trackbar, window_name);
+           
+            // Convertendo a imagem para outro tipo de dados com ganho e deslocamento automáticos
+            output_image.convertTo(output_image, -1, contrast_value, brightness_value);
+        }
 
 
-        imshow("Original", is_gray ? output_image : output_image);
+        if(is_sobel){
+            cv::Mat gradX, gradY, gradient;
+                  // Aplica o operador Sobel nos eixos X e Y
+            cv::Sobel(img_gray, gradX, CV_16S, 1, 0, 3);
+            cv::Sobel(img_gray, gradY, CV_16S, 0, 1, 3);
+
+            // Calcula o gradiente total usando a magnitude
+            cv::convertScaleAbs(gradX, gradX);
+            cv::convertScaleAbs(gradY, gradY);
+            cv::addWeighted(gradX, 0.5, gradY, 0.5, 0, gradient);
+            output_image = gradient;
+        }
+
+        if(is_reduced){
+            int height = output_image.rows;
+            int width = output_image.cols;
+            int layers = output_image.channels();
+
+            int new_h = static_cast<int>(height / 2);
+            int new_w = static_cast<int>(width / 2);
+
+            cv::Mat resized_frame;
+            cv::resize(output_image, resized_frame, cv::Size(new_w, new_h));
+            
+            imshow(window_name, resized_frame);
+        }else{
+            
+        imshow(window_name, output_image);
+
+        }
 
         int pressed_key = cv::waitKey(1);
         if (pressed_key == 27) break;
@@ -93,11 +172,9 @@ int main(int argc, char** argv)
             is_gray = !is_gray;
         }
          else if (pressed_key == 'c' || pressed_key == 'C') {
-            cout << "G was pressed " << endl;
+            cout << "C was pressed " << endl;
             is_canny = !is_canny;
         }
-        
-
 
         else if (pressed_key == 'h' || pressed_key == 'H') {
             cout << "H was pressed " << endl;
@@ -116,7 +193,28 @@ int main(int argc, char** argv)
 
         else if (pressed_key == 'r' || pressed_key == 'R') {
             cout << "R was pressed " << endl;
+            is_rotated = !is_rotated;
         }
+
+        else if (pressed_key == 'a' || pressed_key == 'A') {
+            cout << "A was pressed " << endl;
+            is_brilho = !is_brilho;
+        }
+
+        else if (pressed_key == 's' || pressed_key == 'S') {
+            cout << "S was pressed " << endl;
+            is_sobel = !is_sobel;
+        }
+         else if (pressed_key == 'n' || pressed_key == 'N') {
+            cout << "N was pressed " << endl;
+            is_negative = !is_negative;
+        }
+
+        else if (pressed_key == 'd' || pressed_key == 'D') {
+            cout << "D was pressed " << endl;
+            is_reduced = !is_reduced;
+        }
+
 
  
       
